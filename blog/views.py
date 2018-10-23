dd1c95b6e25127c654fa3ba042bbdab6c4aa5aa0
Abe Pena <pena.abe@gmail.com>
2018-10-23 16:16:34 -0400

    added resume to blog contect

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 from jobs.models import Resume
 # Create your views here.
+
 def blog(request):
     blogposts = Blog.objects.all()
     resume = Resume.objects.filter(pk=1).first()
-    return render(request, 'blog/blog_posts.html', {'blogposts': blogposts})
+    return render(request, 'blog/blog_posts.html', 
+                    {'blogposts': blogposts, 'resume': resume})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     resume = Resume.objects.filter(pk=1).first()
-    return render(request, 'blog/detail.html', {'blog': blog})
+    return render(request, 'blog/detail.html', 
+                    {'blog': blog, 'resume': resume})
